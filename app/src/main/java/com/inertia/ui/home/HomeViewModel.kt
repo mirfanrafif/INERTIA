4c784b3bb6601fe2e7eeefba96b804da23cb9cb9
Irfan Rafif <mirfanrafif17@gmail.com>
2021-06-28 08:11:42 +0700

    app component

--- app/src/main/java/com/inertia/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/inertia/ui/home/HomeViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import androidx.lifecycle.ViewModel
 import com.inertia.core.repository.bencana.IBencanaRepository
 import com.inertia.core.repository.cuaca.CuacaRepository
+import com.inertia.core.repository.cuaca.ICuacaRepository
 import com.inertia.core.repository.user.IUserRepository
 import com.inertia.utils.LocationProvider
 import javax.inject.Inject
@@ -11,7 +12,7 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
     private val bencanaRepository: IBencanaRepository,
     private val userRepository: IUserRepository,
-    private val cuacaRepository: CuacaRepository
+    private val cuacaRepository: ICuacaRepository
 ): ViewModel() {
     fun getUser() = userRepository.getUser()
 
